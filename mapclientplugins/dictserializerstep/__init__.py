303e42a390d73635bdf2ad1566296d3682f4c33d
Hugh Sorby <h.sorby@auckland.ac.nz>
2015-05-18 15:31:34 +1200

    Initial commit of skeleton step.

---
+++ mapclientplugins/dictserializerstep/__init__.py
@@ -0,0 +1,13 @@
+
+'''
+MAP Client Plugin
+'''
+
+__version__ = '0.1.0'
+__author__ = 'Hugh SOrby'
+__stepname__ = 'Dict Serializer'
+__location__ = 'https://github.com/mapclient-plugins/dictserializer'
+
+# import class that derives itself from the step mountpoint.
+from mapclientplugins.dictserializerstep import step
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import sys, os
+
+dependencies = ['''Insert plugin dependencies here''']
+
+setup(name=u'mapclientplugins.dictserializerstep',
+      version='0.0',
+      description='',
+      long_description="",
+      classifiers=[],
+      author=u'Hugh SOrby',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup',]),
+      namespace_packages=['mapclientplugins'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      )
